2a4218736f0aa7b703a68f53e72085509d2917cd
Hossam Khalil <hossamkhalil001@gmail.com>
2021-04-16 00:20:12 +0200

    Feature(db-schema): created tables with OneToMany relations

---
+++ database/migrations/2021_04_15_221614_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('notes');
+            $table->enum('status', ['processing', 'out', 'done'])
+                ->default('processing');
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
