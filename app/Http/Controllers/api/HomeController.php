434f9d3363e1557edfbfaf67ec667f77333c5ccf
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-16 23:56:49 +0200

    Feature(user-home-page): implemented index function for HomeController

--- app/Http/Controllers/api/HomeController.php
+++ app/Http/Controllers/api/HomeController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     function index()
     {
+        // change later to get authenticated user
+        $user = User::find(1);
         $products = Product::all();
-        return $products;
+
+        $latest_order = Order::latest()->first();
+
+        $data["products"] = $products;
+        $data["latest_order"] = $latest_order->products;
+
+        return $data;
     }
 }
