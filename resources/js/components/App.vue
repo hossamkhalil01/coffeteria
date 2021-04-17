be58c4eb79b9089f99eb7f0393eab344f91a386e
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-17 22:13:54 +0200

    Feature(user-home-page): modified app.vue navbar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,43 +1,57 @@
 <template>
-  <div class="">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="collapse navbar-collapse">
-        <div class="navbar-nav">
-          <router-link to="/" class="nav-item nav-link">Home</router-link>
-          <router-link to="/order" class="nav-item nav-link"
-            >Orders</router-link
-          >
-        </div>
-      </div>
-      <div class="navbar-nav">
-        <form action="/logout" method="post">
-          <input type="hidden" name="_token" :value="csrf" />
-          <button type="submit" class="btn btn-primary">Logout</button>
-        </form>
-      </div>
-    </nav>
-    <router-view />
-  </div>
+    <div class="">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="/" class="nav-item nav-link"
+                        >Home</router-link
+                    >
+                    <router-link to="/order" class="nav-item nav-link"
+                        >Orders</router-link
+                    >
+                </div>
+            </div>
+            <div class="navbar-nav align-items-baseline">
+                <div class="nav-item me-3">
+                    <img class="profile-img" :src="getAvatar()" />
+                </div>
+                <form action="/logout" method="post">
+                    <input type="hidden" name="_token" :value="csrf" />
+                    <button type="submit" class="btn btn-secondary btn-sm me-3">
+                        Logout
+                    </button>
+                </form>
+            </div>
+        </nav>
+        <router-view />
+    </div>
 </template>
 
 <script>
 import { csrf, user } from "../main.js";
 
 export default {
-  mounted() {
-    console.log("App main component mounted.");
-  },
-  data() {
-    return {
-      csrf: csrf,
-      user: user,
-    };
-  },
-
-  methods: {
-    getAvatar() {
-      return "storage/avatars/" + this.user.id + "_" + this.user.avatar;
+    mounted() {
+        console.log("App main component mounted.");
     },
-  },
+    data() {
+        return {
+            csrf: csrf,
+            user: user,
+        };
+    },
+
+    methods: {
+        getAvatar() {
+            return this.user.avatar;
+        },
+    },
 };
 </script>
+<style scoped>
+.profile-img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+}
+</style>
