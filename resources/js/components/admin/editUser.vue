48e10e0d3fb3fdd7ce7324c0090852e4b98dc95d
Maymaher <mai.maher003@gmail.com>
2021-04-17 23:26:47 -0400

    Feature(feat-admin-get-users):adding edit user vue component

---
+++ resources/js/components/admin/editUser.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="container">
+<div class="row">
+<form>
+    <div class="form-group">
+    <lable>Name</lable> 
+    <input type="text" class="form-control" />
+    </div>
+
+
+    <div class="form-group">
+    <lable>email</lable> 
+    <input type="email" class="form-control"/>
+    </div>
+
+<div class="form-group">
+    <lable>Room</lable> 
+    <input type="text" class="form-control" />
+</div>
+ <button type="submit" class="btn btn-primary">Update</button>
+</form>
+</div>
+</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            user: []
+        }
+        ,
+        methods:{
+            updateUser(){
+                
+            }
+        }
+    }
+</script>
