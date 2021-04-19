1fed20b209857458d12b02ca683e17ae6a581880
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-19 15:16:57 +0200

    Feature(user-home-page): altered notes column in orders table to be nullable

--- database/migrations/2021_04_19_131131_alter_column_notes_in_orders_table.php
+++ database/migrations/2021_04_19_131131_alter_column_notes_in_orders_table.php
@@ -14,7 +14,7 @@ class AlterColumnNotesInOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->text('notes')->nullable()->change();
         });
     }
 
