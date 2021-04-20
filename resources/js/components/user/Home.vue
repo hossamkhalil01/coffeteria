1752652cfcf79c9b13ddbff25c00dce5cb1ed94a
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-20 20:24:28 +0200

    Feature(admin-home-page): displayed product images in homepage

--- resources/js/components/user/Home.vue
+++ resources/js/components/user/Home.vue
@@ -1,60 +1,70 @@
 <template>
-  <div class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <!-- start of latest order section -->
+    <div class="container-fluid">
         <div class="row justify-content-center">
-          <h3 class="fw-bold text-center mt-3">Latest Order</h3>
-          <div
-            class="col-lg-2 mt-3"
-            v-for="order_product in latest_order"
-            :key="order_product.id"
-          >
-            <img
-              src="https://www.telegraph.co.uk/content/dam/health-fitness/2020/01/09/TELEMMGLPICT000169578515_trans_NvBQzQNjv4BqbTl4D02iCM3NuMfK2RT0HTjsyN2j3JnAYXPi059mk8g.jpeg"
-              alt=""
-            />
-            <p class="product-name text-center">
-              {{ order_product.name }}
-            </p>
-          </div>
-        </div>
-        <!-- end latest order section -->
-        <div class="row justify-content-center">
-          <hr class="text-center w-50" />
-        </div>
+            <div class="col-md-6">
+                <!-- start of latest order section -->
+                <div class="row justify-content-center">
+                    <h3 class="fw-bold text-center mt-3">Latest Order</h3>
+                    <div
+                        class="col-lg-2 mt-3"
+                        v-for="order_product in latest_order"
+                        :key="order_product.id"
+                    >
+                        <img
+                            :src="
+                                'http://localhost:8000/storage/img/' +
+                                order_product.image
+                            "
+                            :alt="order_product.name"
+                        />
+                        <p class="product-name text-center">
+                            {{ order_product.name }}
+                        </p>
+                    </div>
+                </div>
+                <!-- end latest order section -->
+                <div class="row justify-content-center">
+                    <hr class="text-center w-50" />
+                </div>
 
-        <!-- start of products section -->
-        <div class="row justify-content-center">
-          <h3 class="fw-bold text-center">Products</h3>
-          <div
-            class="col-lg-2 mt-3"
-            v-for="product in products"
-            :key="product.id"
-          >
-            <a href="#" @click.prevent="addOrderedProducts(product)">
-              <img
-                :src="'http://localhost:8000/storage/img/' + product.image"
-                :alt="product.name"
-              />
-              <span class="badge rounded-pill bg-info text-dark">{{
-                currencyFormatter(product.price)
-              }}</span>
-            </a>
-            <p class="product-name text-center">
-              {{ product.name }}
-            </p>
-          </div>
+                <!-- start of products section -->
+                <div class="row justify-content-center">
+                    <h3 class="fw-bold text-center">Products</h3>
+                    <div
+                        class="col-lg-2 mt-3"
+                        v-for="product in products"
+                        :key="product.id"
+                    >
+                        <a
+                            href="#"
+                            @click.prevent="addOrderedProducts(product)"
+                        >
+                            <img
+                                :src="
+                                    'http://localhost:8000/storage/img/' +
+                                    product.image
+                                "
+                                :alt="product.name"
+                            />
+                            <span
+                                class="badge rounded-pill bg-info text-dark"
+                                >{{ currencyFormatter(product.price) }}</span
+                            >
+                        </a>
+                        <p class="product-name text-center">
+                            {{ product.name }}
+                        </p>
+                    </div>
+                </div>
+                <!-- end of products section -->
+            </div>
+            <!-- start of order section -->
+            <div class="col-md-5">
+                <newordercomponent v-bind:orderedproducts="orderedProducts" />
+            </div>
+            <!-- end of order section -->
         </div>
-        <!-- end of products section -->
-      </div>
-      <!-- start of order section -->
-      <div class="col-md-5">
-        <newordercomponent v-bind:orderedproducts="orderedProducts" />
-      </div>
-      <!-- end of order section -->
     </div>
-  </div>
 </template>
 
 <script>
@@ -64,76 +74,78 @@ import axios from "axios";
 import newordercomponent from "@components/user/newOrder";
 
 export default {
-  mounted() {
-    console.log("Home component mounted.");
-  },
-  data() {
-    return {
-      csrf: csrf,
-      user: user,
-      products: [],
-      latest_order: [],
-      orderedProducts: [],
-    };
-  },
-  components: {
-    newordercomponent,
-  },
-  created() {
-    axios.get("http://127.0.0.1:8000/api/" + this.user.id).then((response) => {
-      this.products = response.data.products;
-      this.latest_order = response.data.latest_order;
-    });
-  },
-  methods: {
-    currencyFormatter(price) {
-      let formatter = Intl.NumberFormat("eg-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 0,
-      });
-      return formatter.format(price);
+    mounted() {
+        console.log("Home component mounted.");
     },
-    addOrderedProducts(product) {
-      const foundProduct = this.orderedProducts.find((orderedProduct) => {
-        return orderedProduct.id === product.id;
-      });
-      if (!foundProduct) {
-        product.quantity = 1;
-        product.product_id = product.id;
-        this.orderedProducts.push(product);
-      }
+    data() {
+        return {
+            csrf: csrf,
+            user: user,
+            products: [],
+            latest_order: [],
+            orderedProducts: [],
+        };
     },
-  },
+    components: {
+        newordercomponent,
+    },
+    created() {
+        axios
+            .get("http://127.0.0.1:8000/api/" + this.user.id)
+            .then((response) => {
+                this.products = response.data.products;
+                this.latest_order = response.data.latest_order;
+            });
+    },
+    methods: {
+        currencyFormatter(price) {
+            let formatter = Intl.NumberFormat("eg-US", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 0,
+            });
+            return formatter.format(price);
+        },
+        addOrderedProducts(product) {
+            const foundProduct = this.orderedProducts.find((orderedProduct) => {
+                return orderedProduct.id === product.id;
+            });
+            if (!foundProduct) {
+                product.quantity = 1;
+                product.product_id = product.id;
+                this.orderedProducts.push(product);
+            }
+        },
+    },
 };
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,200&display=swap");
 
 .container-fluid {
-  font-family: "Source Sans Pro", sans-serif;
+    font-family: "Source Sans Pro", sans-serif;
 }
 
 img {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-  display: block;
-  margin: auto;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    display: block;
+    margin: auto;
 }
 .badge {
-  position: relative;
-  top: -65%;
-  right: -60%;
-  font-size: 15px;
+    position: relative;
+    top: -65%;
+    right: -60%;
+    font-size: 15px;
 }
 
 .product-name {
-  font-family: "Source Sans Pro", sans-serif;
-  font-weight: 300;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 300;
 }
 
 .bg-info {
-  background-color: #1edaff !important;
+    background-color: #1edaff !important;
 }
 </style>
--- resources/js/components/user/newOrder.vue
+++ resources/js/components/user/newOrder.vue
@@ -1,110 +1,130 @@
 <template>
-  <div class="container-fluid mt-3 border">
-    <div class="row justify-content-center">
-      <h3 class="fw-bold text-center">New Order</h3>
-      <form
-        @submit.prevent="storeOrder"
-        method="POST"
-        class="need-validation"
-        novalidate
-      >
-        <div
-          class="row mb-3"
-          v-for="product in orderedproducts"
-          :key="product.id"
-        >
-          <label :for="'product-' + product.id" class="col-sm-2 col-form-label">
-            {{ product.name }}
-          </label>
-          <div class="col-sm-4">
-            <input
-              type="text"
-              :name="'product-' + product.id"
-              :id="'product-' + product.id"
-              class="form-control"
-              v-model="product.quantity"
-              @change="checkProductQuantity(product)"
-            />
-          </div>
-          <div class="col-sm-4">
-            <a href="#" @click.prevent="increaseProductQuantity(product)">+</a>
-            <a
-              href="#"
-              @click.prevent="decreaseProductQunatity(product)"
-              class="ms-3"
-              >-</a
-            >
-            <p style="display: inline" class="ms-3">
-              {{ product.price }}
-            </p>
-            <a
-              href="#"
-              @click.prevent="removeFromOrderedProducts(product)"
-              class="ms-3"
-              >delete</a
-            >
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-          <div
-            class="col-sm-8 alert alert-danger p-1 mt-2"
-            v-if="errors.ordered_products"
-          >
-            Please add products to order.
-          </div>
-        </div>
-        <div class="row mb-3">
-          <label for="notes" class="col-sm-2 col-form-label">Notes</label>
-          <div class="col-sm-8">
-            <textarea
-              name="notes"
-              id="notes"
-              cols="10"
-              rows="5"
-              class="form-control"
-              v-model="notes"
-            ></textarea>
-          </div>
-        </div>
-        <div class="row mb-3">
-          <label class="col-sm-2 col-form-label" for="rooms">Rooms</label>
-          <div class="col-sm-8">
-            <select
-              class="form-select"
-              id="rooms"
-              v-model="room_id"
-              required
-              @change="removeRoomError"
-            >
-              <option selected disabled>Choose A Room</option>
-              <option v-for="room in rooms" :key="room.id" :value="room.id">
-                {{ room.land_mark }} - {{ room.number }}
-              </option>
-            </select>
-            <div class="alert alert-danger p-1 mt-2" v-if="errors.room_id">
-              Please select a room.
-            </div>
-          </div>
-        </div>
+    <div class="container-fluid mt-3 border">
         <div class="row justify-content-center">
-          <hr class="text-center" />
+            <h3 class="fw-bold text-center">New Order</h3>
+            <form
+                @submit.prevent="storeOrder"
+                method="POST"
+                class="need-validation"
+                novalidate
+            >
+                <div
+                    class="row mb-3"
+                    v-for="product in orderedproducts"
+                    :key="product.id"
+                >
+                    <label
+                        :for="'product-' + product.id"
+                        class="col-sm-2 col-form-label"
+                    >
+                        {{ product.name }}
+                    </label>
+                    <div class="col-sm-4">
+                        <input
+                            type="text"
+                            :name="'product-' + product.id"
+                            :id="'product-' + product.id"
+                            class="form-control"
+                            v-model="product.quantity"
+                            @change="checkProductQuantity(product)"
+                        />
+                    </div>
+                    <div class="col-sm-4">
+                        <a
+                            href="#"
+                            @click.prevent="increaseProductQuantity(product)"
+                            >+</a
+                        >
+                        <a
+                            href="#"
+                            @click.prevent="decreaseProductQunatity(product)"
+                            class="ms-3"
+                            >-</a
+                        >
+                        <p style="display: inline" class="ms-3">
+                            {{ product.price }}
+                        </p>
+                        <a
+                            href="#"
+                            @click.prevent="removeFromOrderedProducts(product)"
+                            class="ms-3"
+                            >delete</a
+                        >
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div
+                        class="col-sm-8 alert alert-danger p-1 mt-2"
+                        v-if="errors.ordered_products"
+                    >
+                        Please add products to order.
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="notes" class="col-sm-2 col-form-label"
+                        >Notes</label
+                    >
+                    <div class="col-sm-8">
+                        <textarea
+                            name="notes"
+                            id="notes"
+                            cols="10"
+                            rows="5"
+                            class="form-control"
+                            v-model="notes"
+                        ></textarea>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label class="col-sm-2 col-form-label" for="rooms"
+                        >Rooms</label
+                    >
+                    <div class="col-sm-8">
+                        <select
+                            class="form-select"
+                            id="rooms"
+                            v-model="room_id"
+                            required
+                            @change="removeRoomError"
+                        >
+                            <option selected disabled>Choose A Room</option>
+                            <option
+                                v-for="room in rooms"
+                                :key="room.id"
+                                :value="room.id"
+                            >
+                                {{ room.land_mark }} - {{ room.number }}
+                            </option>
+                        </select>
+                        <div
+                            class="alert alert-danger p-1 mt-2"
+                            v-if="errors.room_id"
+                        >
+                            Please select a room.
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <hr class="text-center" />
+                </div>
+                <div class="row mb-3 justify-content-end">
+                    <div class="col-sm-5">
+                        <p>
+                            Total Price:
+                            {{ currencyFormatter(totalOrderPrice) }}
+                        </p>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center mb-3">
+                    <div class="col-sm-3">
+                        <button type="submit" class="btn btn-primary">
+                            Order
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="row mb-3 justify-content-end">
-          <div class="col-sm-5">
-            <p>
-              Total Price:
-              {{ currencyFormatter(totalOrderPrice) }}
-            </p>
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center mb-3">
-          <div class="col-sm-3">
-            <button type="submit" class="btn btn-primary">Order</button>
-          </div>
-        </div>
-      </form>
     </div>
-  </div>
 </template>
 
 <script>
@@ -113,142 +133,148 @@ import * as user from "@helpers/currentUser.js";
 import axios from "axios";
 
 export default {
-  mounted() {
-    console.log("newOrder component mounted.");
-  },
-  data() {
-    return {
-      csrf: csrf,
-      user: user,
-      rooms: [],
-      totalPrice: 0,
-      room_id: null,
-      notes: "",
-      success: false,
-      error: false,
-      errors: {},
-    };
-  },
-  props: {
-    orderedproducts: Array,
-  },
-  updated() {
-    if (this.orderedproducts.length > 0) {
-      delete this.errors.ordered_products;
-    }
-  },
-  created() {
-    axios.get("http://127.0.0.1:8000/api/rooms").then((response) => {
-      this.rooms = response.data;
-    });
-  },
-  methods: {
-    getAvatar() {
-      return this.user.avatar;
+    mounted() {
+        console.log("newOrder component mounted.");
     },
-    currencyFormatter(price) {
-      let formatter = Intl.NumberFormat("eg-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 0,
-      });
-      return formatter.format(price);
+    data() {
+        return {
+            csrf: csrf,
+            user: user,
+            rooms: [],
+            totalPrice: 0,
+            room_id: null,
+            notes: "",
+            success: false,
+            error: false,
+            errors: {},
+        };
     },
-    increaseProductQuantity(product) {
-      const newQuantity = product.quantity + 1;
-      if (newQuantity >= 100) {
-        product.quantity = 100;
-      } else {
-        product.quantity++;
-      }
+    props: {
+        orderedproducts: Array,
     },
-    decreaseProductQunatity(product) {
-      const newQuantity = product.quantity - 1;
-      if (newQuantity === 0) {
-        const removeProductIndex = this.orderedproducts.findIndex(
-          (orderedProduct) => {
-            return orderedProduct.id === product.id;
-          }
-        );
-        this.orderedproducts.splice(removeProductIndex, 1);
-      } else {
-        product.quantity = newQuantity;
-      }
-    },
-    checkProductQuantity(product) {
-      if (product.quantity <= 0) {
-        const removeProductIndex = this.orderedproducts.findIndex(
-          (orderedProduct) => {
-            return orderedProduct.id === product.id;
-          }
-        );
-        this.orderedproducts.splice(removeProductIndex, 1);
-      } else if (product.quantity >= 100) {
-        product.quantity = 100;
-      }
-    },
-    removeFromOrderedProducts(product) {
-      const removeProductIndex = this.orderedproducts.findIndex(
-        (orderedProduct) => {
-          return orderedProduct.id === product.id;
+    updated() {
+        if (this.orderedproducts.length > 0) {
+            delete this.errors.ordered_products;
         }
-      );
-      this.orderedproducts.splice(removeProductIndex, 1);
     },
-    storeOrder() {
-      let ordered_products = [];
-      this.orderedproducts.forEach((ordered_product) => {
-        let product = {};
-        product.product_id = ordered_product.id;
-        product.quantity = ordered_product.quantity;
-        ordered_products.push(product);
-      });
-
-      const formData = {
-        room_id: this.room_id,
-        notes: this.notes,
-        ordered_products: ordered_products,
-        total_price: this.totalPrice,
-      };
-      axios
-        .post("http://127.0.0.1:8000/api/" + this.user.id + "/orders", formData)
-        .then((res) => {
-          this.onSuccess(res.data.message);
-        })
-        .catch((error) => {
-          this.onFailure(error.response.data);
+    created() {
+        axios.get("http://127.0.0.1:8000/api/rooms").then((response) => {
+            this.rooms = response.data;
         });
     },
-    onSuccess(message) {
-      this.$router.push({ name: "Order" });
+    methods: {
+        getAvatar() {
+            return this.user.avatar;
+        },
+        currencyFormatter(price) {
+            let formatter = Intl.NumberFormat("eg-US", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 0,
+            });
+            return formatter.format(price);
+        },
+        increaseProductQuantity(product) {
+            const newQuantity = product.quantity + 1;
+            if (newQuantity >= 100) {
+                product.quantity = 100;
+            } else {
+                product.quantity++;
+            }
+        },
+        decreaseProductQunatity(product) {
+            const newQuantity = product.quantity - 1;
+            if (newQuantity === 0) {
+                const removeProductIndex = this.orderedproducts.findIndex(
+                    (orderedProduct) => {
+                        return orderedProduct.id === product.id;
+                    }
+                );
+                this.orderedproducts.splice(removeProductIndex, 1);
+            } else {
+                product.quantity = newQuantity;
+            }
+        },
+        checkProductQuantity(product) {
+            if (product.quantity <= 0) {
+                const removeProductIndex = this.orderedproducts.findIndex(
+                    (orderedProduct) => {
+                        return orderedProduct.id === product.id;
+                    }
+                );
+                this.orderedproducts.splice(removeProductIndex, 1);
+            } else if (product.quantity >= 100) {
+                product.quantity = 100;
+            }
+        },
+        removeFromOrderedProducts(product) {
+            const removeProductIndex = this.orderedproducts.findIndex(
+                (orderedProduct) => {
+                    return orderedProduct.id === product.id;
+                }
+            );
+            this.orderedproducts.splice(removeProductIndex, 1);
+        },
+        storeOrder() {
+            let ordered_products = [];
+            this.orderedproducts.forEach((ordered_product) => {
+                let product = {};
+                product.product_id = ordered_product.id;
+                product.quantity = ordered_product.quantity;
+                ordered_products.push(product);
+            });
+
+            const formData = {
+                room_id: this.room_id,
+                notes: this.notes,
+                ordered_products: ordered_products,
+                total_price: this.totalPrice,
+            };
+            axios
+                .post(
+                    "http://127.0.0.1:8000/api/" + this.user.id + "/orders",
+                    formData
+                )
+                .then((res) => {
+                    this.onSuccess(res.data.message);
+                })
+                .catch((error) => {
+                    this.onFailure(error.response.data);
+                });
+        },
+        onSuccess(message) {
+            this.$router.push({ name: "UserOrder" });
+        },
+        onFailure(errorData) {
+            this.errors = errorData.errors;
+            this.error = true;
+        },
+        removeRoomError() {
+            delete this.errors.room_id;
+        },
     },
-    onFailure(errorData) {
-      this.errors = errorData.errors;
-      this.error = true;
+    computed: {
+        totalOrderPrice() {
+            this.totalPrice = this.orderedproducts.reduce(
+                (sum, orderedProduct) => {
+                    return sum + orderedProduct.price * orderedProduct.quantity;
+                },
+                0
+            );
+            return this.totalPrice;
+        },
     },
-    removeRoomError() {
-      delete this.errors.room_id;
-    },
-  },
-  computed: {
-    totalOrderPrice() {
-      this.totalPrice = this.orderedproducts.reduce((sum, orderedProduct) => {
-        return sum + orderedProduct.price * orderedProduct.quantity;
-      }, 0);
-      return this.totalPrice;
-    },
-  },
 };
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,600;0,200&display=swap");
 
 .container-fluid {
-  font-family: "Source Sans Pro", sans-serif;
-  font-weight: 600;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 600;
 }
 
 .border {
-  border-radius: 2%;
+    border-radius: 2%;
 }
 </style>
