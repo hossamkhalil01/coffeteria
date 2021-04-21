3626ada0b850a40cc2ee3629bd393925bf01e144
leenasherif239 <sherifleena@yahoo.com>
2021-04-21 13:51:16 +0200

    (feat-products):edit-is_available

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -79,8 +79,8 @@ class ProductController extends Controller
             $product  = Product::findOrfail($id);
             $product->name = $request->get('name');
             $product->price = $request->get('price');
-            // $product->is_available = $request->get('is_available');
-            $product->category_id = $request->get('category_id');
+            $product->is_available = $request->get('is_available');
+            // $product->category_id = $request->get('category_id');
          
            
             // console.log($product);
