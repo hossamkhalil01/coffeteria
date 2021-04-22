1cd769310f0b6aa2499c3a56f57bc1b8674c049c
leenasherif239 <sherifleena@yahoo.com>
2021-04-22 20:07:24 +0200

    (feat-products):add validation for price in create product

--- resources/js/components/admin/CreateProduct.vue
+++ resources/js/components/admin/CreateProduct.vue
@@ -73,9 +73,9 @@
     <button type="Reset" class="btn btn-primary">Reset</button>
    <br/>
       <span v-if="allerros.name" :class="[' alert alert-danger']">@{{ allerros.name[0] }}</span>
-       <!-- <span v-if="allerros.price" :class="[' alert alert-danger']">@{{ allerros.price[0] }}</span>
-        <span v-if="allerros.image" :class="[' alert alert-danger']">@{{ allerros.image[0] }}</span>
-              <span v-if="allerros.category_id" :class="[' alert alert-danger']">@{{ allerros.category_id[0] }}</span> -->
+       <span v-if="allerros.price" :class="[' alert alert-danger']">@{{ allerros.price[0] }}</span> 
+        <!-- <span v-if="allerros.image" :class="[' alert alert-danger']">@{{ allerros.image[0] }}</span>
+              <span v-if="allerros.category_id" :class="[' alert alert-danger']">@{{ allerros.category_id[0] }}</span>  -->
     <!-- </div> -->
   </form>
   <!-- </div> -->
