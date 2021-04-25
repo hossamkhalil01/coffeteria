1e10924fb2f9eb92e78b2e644e800deab4e12ae5
Hossam Khalil <hossamkhalil001@gmail.com>
2021-04-25 06:37:37 +0200

    Feature(auth-styling): added social buttons to the form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,21 +5,22 @@
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Tangerine:400,700" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 @endpush
-
 @section('content')
     <!--header-->
     <div class="header-w3l">
         <h1 style="background-color: rgba(100, 109, 100, 0.835)">
             <span>W</span>elcome
             <span>B</span>ack
+
         </h1>
     </div>
     <!--//header-->
     <div class="main-content-agile">
         <div class="sub-main-w3">
-            <img src="images/login/logo_cropped.jpg" alt="logo" width="100" height="100" />
-            <h2>Coffeteria</h2>
+            <h1 class="fs-1 fst-italic text-light mb-5 ">Sign in</h1>
+
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="pom-agile">
@@ -52,19 +53,41 @@
                 <div class="right-w3l">
                     <input type="submit" value="Login">
                 </div>
-                <div class="forgot-w3l">
-                    <a href="{{ route('password.request') }}">Forgot Password?</a>
-                </div>
             </form>
+            <div class="forgot-w3l">
+                <a href="{{ route('password.request') }}">Forgot Password?</a>
+            </div>
+
+            <div class="row mt-3 mb-4 align-items-baseline">
+                <hr class="col-5 text-light fw-bolder or-line">
+                <p class="col-2 text-center fs-4 text-light">Or</p>
+                <hr class="col-5  text-light fw-bolder or-line">
+            </div>
+
+            {{-- Social login section --}}
+            <div class="row mt-3 justify-content-center">
+                <a href="{{ route('social.oauth', 'facebook') }}"
+                    class="btn btn-primary fa fw-bolder fa-facebook fs-3 col-2 me-3" role="button"></a>
+                <a href="{{ route('social.oauth', 'google') }}"
+                    class="btn btn-danger fa fw-bolder fa-google fs-3 col-2 me-3" role="button"></a>
+                <a href="{{ route('social.oauth', 'github') }}" class="btn btn-dark fa fw-bolder fa-github fs-3 col-2 "
+                    role="button"></a>
+            </div>
+            {{-- End of social section --}}
+
+            <div class="sign-up mt-5 mb-0">
+                <p class="text-light">Don't have an account? <a class="forgot-w3l" href="{{ route('register') }}">
+                        Register</a></p>
+            </div>
         </div>
     </div>
     <!--//main-->
     <!--footer-->
-    <div class="footer">
+    {{-- <div class="footer">
         <p>&copy; 2018 Portrait Login Form. All rights reserved | Design by
             <a href="http://w3layouts.com">W3layouts</a>
         </p>
-    </div>
+    </div> --}}
     <!--//footer-->
 @endsection
 
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -26,7 +26,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -78,7 +78,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
 
         <main class="py-4">
             @yield('content')
