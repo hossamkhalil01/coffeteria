465b0ae99a92106db897689c2bc4036a39192639
Hossam Khalil <hossamkhalil001@gmail.com>
2021-04-25 09:25:00 +0200

    Feature(admin-role): added gaurd to all api routes with auth middleware

--- routes/api.php
+++ routes/api.php
@@ -21,39 +21,39 @@ use App\Http\Controllers\imagecontroller;
 |
 */
 
+Route::middleware(['auth'])->group(function () {
+
+    Route::apiResource("/products", ProductController::class);
+
+    Route::get('/categories', [ProductController::class, 'getCategories']);
+
+    Route::post('/addCategory', [ProductController::class, 'addCategory']);
+
+    // orders routes
+    Route::get("{user_id}/orders/range", [OrderController::class,  "get_orders_within_date_range"])->name("orders.range");
+    Route::apiResource("{user_id}/orders", OrderController::class);
+    // home routes
+    Route::get("/admin/index", [HomeController::class, "admin_index"])->name("admin_index");
+    Route::get("/rooms", [HomeController::class, "get_rooms"])->name("get_rooms");
+    // checks
+    Route::get("/orders", [OrderController::class, "get_orders"]);
+    Route::get("/orders/{id}", [OrderController::class, "get_order"]);
+    Route::apiResource("/users", UserController::class);
+
+    Route::get("/{user_id}", [HomeController::class, "index"])->name("index");
+    Route::get('/admin/getusers', [admincontroller::class, 'GetAllUsers']);
+
+    Route::delete('/admin/deleteuser/{id}', [admincontroller::class, 'destroy']);
+
+    Route::patch('/admin/edituser/{id}', [admincontroller::class, 'update']);
+
+    Route::get('/admin/getusers/{id}', [admincontroller::class, 'showuser']);
+    Route::post('/admin/create', [admincontroller::class, 'store']);
+
+    Route::post('upload', [imagecontroller::class, 'upload']);
+    Route::resource('/rooms', roomcontroller::class);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::apiResource("/products", ProductController::class);
-
-Route::get('/categories', [ProductController::class, 'getCategories']);
-
-Route::post('/addCategory', [ProductController::class, 'addCategory']);
-
-// orders routes
-Route::get("{user_id}/orders/range", [OrderController::class,  "get_orders_within_date_range"])->name("orders.range");
-Route::apiResource("{user_id}/orders", OrderController::class);
-// home routes
-Route::get("/admin/index", [HomeController::class, "admin_index"])->name("admin_index");
-Route::get("/rooms", [HomeController::class, "get_rooms"])->name("get_rooms");
-
-
-// checks
-Route::get("/orders", [OrderController::class, "get_orders"]);
-Route::get("/orders/{id}", [OrderController::class, "get_order"]);
-Route::apiResource("/users", UserController::class);
-
-Route::get("/{user_id}", [HomeController::class, "index"])->name("index");
-Route::get('/admin/getusers', [admincontroller::class, 'GetAllUsers']);
-
-Route::delete('/admin/deleteuser/{id}', [admincontroller::class, 'destroy']);
-
-Route::patch('/admin/edituser/{id}', [admincontroller::class, 'update']);
-
-Route::get('/admin/getusers/{id}', [admincontroller::class, 'showuser']);
-Route::post('/admin/create', [admincontroller::class, 'store']);
-
-Route::post('upload', [imagecontroller::class, 'upload']);
-Route::resource('/rooms', roomcontroller::class);
