487afa95889ca1be496e0dc7580f8085acbb79f3
Maymaher <mai.maher003@gmail.com>
2021-04-25 21:46:15 -0400

    Feature(feat-admin-orders-view):updating orders style

--- resources/js/components/admin/Order.vue
+++ resources/js/components/admin/Order.vue
@@ -4,7 +4,10 @@
     <!-- Start of checks table-->
     <div class="col-6 mb-3">
         <h1>Orders</h1>
-        <table class="table table-hover" style="border: 1px solid" v-for="order in orders">
+
+        <div v-for="order in orders">
+        
+        <table class="table table-hover" style="border: 1px solid;margin: 2px 0px;" >
             <thead>
                 <tr>
                     <th class="table-primary">Order Date</th>
@@ -38,29 +41,32 @@
 
                 </tr>
 
-                <div style="display:inline-block">
-                    <tr >
+               
+            </tbody>
+        </table>
+
+
+         <div style="display:inline-block;border:1px solid" class="col-12 mb-3">
+                    <div class="product-container">
                         <td  v-for="product in order.products" >
-                            <img :src="`http://localhost:8000/storage/img/${product.image}`" style="width:100px">
-                           <br/>
-                            {{product.name}}
-                            <br/>
-                            {{product.pivot.quantity}}
+                           <img  :src="`http://localhost:8000/storage/img/${product.image}`" class="product-image" >
+                                                     <div class="price"> {{product.price}} LE</div>
+
+                           <p class="productname">  {{product.name}}</p>
+                           
+                            <p class="productquantity">{{product.pivot.quantity}}</p>
 
 
                         </td>
 
-                    <h3 style="float:right">Total: {{order.total_price}}</h3>
+                   
 
-                    </tr>
-
-
+                    </div>
+ <h4 style="float:right;margin-right: 37px;">Total: EGP {{order.total_price}}</h4>
                    
                 </div>
 
-            </tbody>
-        </table>
-
+</div>
     </div>
     <!-- End of checks table -->
 </div>
@@ -113,3 +119,39 @@ export default {
     }
 };
 </script>
+
+
+<style>
+.product-image{
+    width: 100px;
+border-radius: 75px;
+height: 93px;
+margin: 0px 13px;
+}
+
+.productname{
+   margin-left: 43px;
+}
+
+.productquantity{
+    margin-left: 53px;
+}
+
+.product-container{
+   
+    margin: 37px;
+}
+
+.price{
+ width: 56px;
+height: 43px;
+border: 1px solid;
+border-radius: 48%;
+text-align: center;
+margin: -90px 77px;
+position: absolute;
+background-color: #eee;
+padding: 9px 3px;
+font-size: 15px;
+}
+</style>
